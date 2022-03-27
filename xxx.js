9784542ec9aa2633bb38b7800d8480e01ca501c4
edhah <edhahsaeedbaamer.com>
2022-03-27 20:19:10 +0300

    frist coomit

---
+++ xxx.js
@@ -0,0 +1,22 @@
+function messag(){
+    var name=document.getElementById("user").value;
+   
+    var pass=document.getElementById("password").value;
+    if(name=="edhah"&& pass=="123"){
+        alert("oooooooooooooooo");
+    window.location.href="edhah.html";
+
+}
+else{
+    alert("pedcnkxm,bhdsvmkv");
+}
+}
+
+var fristname=6;
+switch(fristname){
+    case 5:
+        document.write("hello edhah");
+        break;
+        default:
+            document.write("plesae enter your name ");
+}
